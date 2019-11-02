ed6f0753c0643a49ab17b4e3efc9ae22161fd3f5
min9nim <min9nim@gmail.com>
2019-11-03 03:46:31 +0900

    add log

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,11 +5,10 @@ import {pipe, complement, filter} from 'ramda'
 const prod_url = 'https://little-jesus-api.now.sh'
 const dev_url = 'https://little-jesus-api-git-develop.min1.now.sh'
 const BASEURL = window.location.host === 'little-jesus-admin.now.sh' ? prod_url : dev_url
-
-const headers = {'Content-Type': 'application/json'}
+console.log({BASEURL})
 
 export async function req(query: any, variables = {}) {
-  let config = {headers}
+  let config = {headers: {'Content-Type': 'application/json'}}
   const result = await axios.post(BASEURL, {query: print(query), variables}, config)
   if (result.data.errors) {
     throw result.data.errors
