29e5b1aab1d70ac05035ccbfbb40b6d4892ba3c0
min9nim <min9nim@gmail.com>
2019-11-20 03:02:41 +0900

    new useHandleSave

--- src/views/point.fn.ts
+++ src/views/point.fn.ts
@@ -1,4 +1,4 @@
-import {qPointMenus} from '@/biz/query'
+import {qPointMenus, qCreatePointMenu} from '@/biz/query'
 import {req} from '@/utils'
 
 export function useBeforeMount({state}) {
@@ -7,5 +7,20 @@ export function useBeforeMount({state}) {
     const result = await req(qPointMenus)
     state.loading = false
     state.menus = result.res
+  }
+}
+
+export function useHandleSave({state}) {
+  return async () => {
+    const priority = Number(state.newPointMenu.priority)
+    if (typeof priority !== 'number') {
+      throw Error('Not a number priority')
+    }
+    state.newPointMenu.loading = true
+    const variables = {...state.newPointMenu, priority}
+    await req(qCreatePointMenu, variables)
+    state.newPointMenu.loading = false
+    state.menus.push(state.newPointMenu)
+    state.newPointMenu = null
   }
 }
