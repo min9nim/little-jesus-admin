8eea2ee4fe98a20343fc24bf58c9ea9046e7a571
min9nim <min9nim@gmail.com>
2019-12-09 23:51:58 +0900

    특정 버젼을 명시한 경우에는 해당 버젼의 prod_url 로 연결

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,8 +4,17 @@ import {pipe, complement, filter} from 'ramda'
 
 const prod_url = 'https://little-jesus-api.now.sh'
 const dev_url = 'https://little-jesus-api-git-develop.min1.now.sh'
-const BASEURL = window.location.host === 'little-jesus-admin.now.sh' ? prod_url : dev_url
-console.log({BASEURL})
+let BASEURL = window.location.host === 'little-jesus-admin.now.sh' ? prod_url : dev_url
+if (window.location.host.indexOf('ver') > 0) {
+  // 특정 버젼을 명시한 경우에는 해당 버젼의 prod_url 로 연결
+  const start = location.host.indexOf('ver')
+  const end = location.host.indexOf('.now.sh')
+  const version = location.host.slice(start, end)
+
+  const [tmp1, tmp2, tmp3] = prod_url.split('.')
+  BASEURL = [tmp1 + '-' + version, tmp2, tmp3].join('.')
+}
+console.info('api server = ' + BASEURL)
 
 export async function req(query: any, variables = {}) {
   let config = {headers: {'Content-Type': 'application/json'}}
